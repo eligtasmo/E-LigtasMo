57ae6c357e037956716ec4394b01531d24a95c93
ryoshken <ryoshken@gmail.com>
2026-05-12 22:42:25 +0800

    chore: remove temporary cjs rewrite scripts and backup tsx files from scratch dir

--- scratch/check_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'api/db.php';
-$stmt = $pdo->query("SHOW TABLES");
-$tables = $stmt->fetchAll(PDO::FETCH_COLUMN);
-file_put_contents('scratch/tables.txt', implode("\n", $tables));
-?>
